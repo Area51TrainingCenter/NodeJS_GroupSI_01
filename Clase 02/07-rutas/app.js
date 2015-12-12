0a163a2e917a79e7b702a56e8f5c4dc6dcbd6f8b
Sergio <sergiohidalgocaceres@gmail.com>
2015-12-12 13:53:40 -0500

    Clase 02

--- Clase 02/07-rutas/app.js
+++ Clase 02/07-rutas/app.js
@@ -1,6 +1,9 @@
 var express = require("express"),
 	app = express(),
 	path = require("path"),
+	morgan = require("morgan"),
+	serveFavicon = require("serve-favicon"),
+	favicon = path.join(__dirname, "/favicon.ico"),
 	motorVistas = "ejs",
 	directorioVistas = path.join(__dirname, "/vistas"),
 	directorioPublico = express.static(path.join(__dirname, "/publico")),
@@ -13,6 +16,8 @@ function fnInicio() {
 app.set("view engine", motorVistas);
 app.set("views", directorioVistas);
 
+app.use(serveFavicon(favicon));
+app.use(morgan("dev"));
 app.use(directorioPublico);
 app.use(rutas);
 

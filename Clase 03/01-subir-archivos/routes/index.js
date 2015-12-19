db48b653d22ce5728afd406f05725dd818f2d5f2
sergio <sergiohidalgocaceres@gmail.com>
2015-12-19 11:07:27 -0500

    Clase 03

---
+++ Clase 03/01-subir-archivos/routes/index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+var formidable = require("formidable");
+var fs = require("fs-extra");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('subir');
+});
+
+router.post("/subir", function(req, res, next){
+
+	function fnRecibidos(err, campos, archivos) {
+		res.send("Archivos recibidos: " + util.inspect({files: archivos}));
+	}
+
+	function fnAvance(recibidos, total) {
+		console.log("Avance: " + recibidos/total*100);
+	}	
+
+	function fnError(err) {
+		console.log(err);
+	}	
+
+	function fnFin(campos, archivos) {
+		var rutaTemporal = this.openedFiles[0].path,
+			nombreArchivo = this.openedFiles[0].name,
+			nuevoDirectorio = "./public/uploads/";
+
+		function fnCopiado(err) {
+			if(err) {
+				console.log(err);
+			}
+		}
+
+		fs.copy(rutaTemporal, nuevoDirectorio + nombreArchivo, fnCopiado)			
+	}
+
+	var form = formidable.IncomingForm();
+
+	form.parse(req, fnRecibidos);
+	form.on("progress", fnAvance);
+	form.on("error", fnError);
+	form.on("end", fnFin);
+
+})
+
+
+
+module.exports = router;
